db33c321f27b40a5d473e2eb2527dff3c6bcc581
Зазунов Денис <99178404+DenisZaz@users.noreply.github.com>
2022-02-27 19:04:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int l = 0, r = size - 1, check = -1, count = 0;
+  int middle;
+  while (l <= r) {
+    middle = l + (r - l) / 2;
+    if (arr[middle] == value) {
+      count++;
+      check = middle;
+      break;
+    }
+      if (value < arr[middle])
+        r = middle - 1;
+      else
+        l = middle + 1;
+  }
+
+  if (check >= 0) {
+    for (int start = 0; start < check; start++) {
+      if (arr[start] == value)
+        count++;
+    }
+    for (int end = size - 1; end > check; end--) {
+      if (arr[end] == value) 
+        count++;
+    }
+  }
+
+  if (count > 0)
+    return count;
+  else
+    return 0; 
 }
